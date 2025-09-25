8bafbacbf380018170c4f05ba622f8db8f262bc0
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-09-25 17:01:28 +0200

    feat: added base for OpenCV

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from pygame_prototype import PGProto
+from opencv_prototype import CVProto, Source
+
+from pathlib import Path
 
 def main() -> None:
-    window: PGProto = PGProto()
+    # window: PGProto | CVProto = PGProto()
+    # window: PGProto | CVProto = CVProto(Source.TEST_SAMPLES)
+    window: PGProto | CVProto = CVProto(Source.OBS, capture_device_id=4)
+    # window: PGProto | CVProto = CVProto(Source.USER_PATH, image_path=Path("./test_samples/full_map.png"))
 
     window.run()
     window.quit()
---
+++ src/utils.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import os
+
+import constants as Constants
+
+def get_images_path(path: Path | str) -> list[Path]:
+    if isinstance(path, str):
+        path = Path(path)
+
+    if not path.exists():
+        raise RuntimeError("Invalid path")
+
+    images: list[Path] = []
+
+    if str(path).endswith(Constants.IMAGE_EXTENSIONS):
+        images.append(path)
+    elif path.is_dir():
+        images.extend(
+                [
+                    path / Path(file)
+                    for file in os.listdir(path)
+                    if file.endswith(Constants.IMAGE_EXTENSIONS) and os.path.isfile(path / file)
+                ]
+            )
+    
+    return images
